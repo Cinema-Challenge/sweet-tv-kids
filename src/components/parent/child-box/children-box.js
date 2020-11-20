a7766def9239ff8f79d94fcd6c69af263ad3756f
andrii-amplify <atykhan@amplify.com>
2020-11-20 11:13:06 +0200

    Add child modal

--- src/components/parent/child-box/children-box.js
+++ src/components/parent/child-box/children-box.js
@@ -13,35 +13,47 @@ const ChildBox = ({ child }) => {
             <span className="child-box__name text">{name}</span>
             <span className="child-box__age text--secondary">{age} Років</span>
           </h4>
-          <div className="child-box__statistic">
-            <div className="child-box__average">
-              <span className="text text--field">Середній час за тиждень:</span>
-              <span className="text text--percent">{weekStatistik.percent}</span>
-              <span className="text text">{weekStatistik.time}</span>
-              <span className="text text--secondary">хв / день</span>
+          {weekStatistik && (
+            <div className="child-box__statistic">
+              <div className="child-box__average">
+                <span className="text text--field">
+                  Середній час за тиждень:
+                </span>
+                <span className="text text--percent">
+                  {weekStatistik.percent}
+                </span>
+                <span className="text text">{weekStatistik.time}</span>
+                <span className="text text--secondary">хв / день</span>
+              </div>
+              <div className="child-box__average">
+                <span className="text text--field">
+                  Середній час за місяць:
+                </span>
+                <span className="text text--percent">
+                  {weekStatistik.percent}
+                </span>
+                <span className="text text">{weekStatistik.time}</span>
+                <span className="text text--secondary">хв / день</span>
+              </div>
             </div>
-            <div className="child-box__average">
-              <span className="text text--field">Середній час за місяць:</span>
-              <span className="text text--percent">{weekStatistik.percent}</span>
-              <span className="text text">{weekStatistik.time}</span>
-              <span className="text text--secondary">хв / день</span>
-            </div>
+          )}
+        </div>
+      </div>
+      {films && (
+        <div className="child-box__bottom">
+          <div className="child-box__film">
+            <img src={films.img} alt="" className="child-box__film-img" />
+            <img src={films.img} alt="" className="child-box__film-img" />
+            <img src={films.img} alt="" className="child-box__film-img" />
+          </div>
+          <div className="child-box__last-film">
+            <p className="text">Останній перегляд:</p>
+            <p className="text text--big">{films.lastFilm}</p>
+            <p className="text text--secondary">укр. озвучка, 2.25 год</p>
+            <p className="text">час: 17:24</p>
           </div>
         </div>
-      </div>
-      <div className="child-box__bottom">
-        <div className="child-box__film">
-          <img src={films.img} alt="" className="child-box__film-img" />
-          <img src={films.img} alt="" className="child-box__film-img" />
-          <img src={films.img} alt="" className="child-box__film-img" />
-        </div>
-        <div className="child-box__last-film">
-          <p className="text">Останній перегляд:</p>
-          <p className="text text--big">{films.lastFilm}</p>
-          <p className="text text--secondary">укр. озвучка, 2.25 год</p>
-          <p className="text">час: 17:24</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
